866d14c71f1cd639b06a71c5a390572780d9e365
Carlos Ignacio Mancón <manconci@gmail.com>
2016-08-18 09:39:27 -0300

    Added header file to workaround compiling error.

--- externals/usblib/src/Drivers/USB/Core/Events.c
+++ externals/usblib/src/Drivers/USB/Core/Events.c
@@ -34,7 +34,7 @@
 #define  __INCLUDE_FROM_EVENTS_C
 #define  __INCLUDE_FROM_USB_DRIVER
 #include "../inc/Drivers/USB/Core/USBMode.h"
-
+#include "../inc/Drivers/USB/Core/Events.h"
 void USB_Event_Stub(void)
 {
 
